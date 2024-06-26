095252087872711edbe7547f73ccb8d2d0752568
Abdirizak Mohamed <inzaghi3650@gmail.com>
2024-06-25 22:13:50 -0500

    Completed Assignment

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -9,9 +9,13 @@ console.log('***** Object Practice *****')
   - Also give it an array 'favThreeFoods'
 */
 const me = {
-  // TODO - add properties here
+ firstName: 'Abdirizak',
+ lastName:' Mohamed',
+ hasSibling: true,
+ shoeCount: 20,
+ favThreeFoods : ['Ugali','Rice', 'Pasta' ]
 };
-console.log('A little about me:', me);
+console.log('A little about me:', me); // An OBJECT with key-value pair
 
 /* 2. Accessing object properties.
   - Create a variable called fullName 
@@ -19,15 +23,27 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
+let fullName = `${me.firstName}${me.lastName}`; //used template literals/Backticks
 
+console.log(fullName); // Abdirizak Mohamed
 
 
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
+    //  first favorite food
+    console.log(me.favThreeFoods[0]); //Ugali
 
+    // Last favorite food 
+    console.log(me.favThreeFoods[2]) // pasta (..using index)
 
+    // Below console.logs are just for TRIAL & ERROR (Doing it differently to get same Ouput) !
+
+    let array = me.favThreeFoods;
+    console.log(array[array.length -1]) // pasta (..using .length method..In case you don't know the length of the array!)
+
+    console.log(me.favThreeFoods[me.favThreeFoods.length -1]) // same ouput as two above .."pasta"
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
@@ -36,6 +52,13 @@ console.log('A little about me:', me);
   - Console.log your updated shoe count. 
 */
 
+   // Current shoeCount
+   console.log(me.shoeCount) // 20
+
+   // Updated shoeCount
+   me.shoeCount = 21;
+   console.log(me.shoeCount); // 21
+
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -43,3 +66,11 @@ console.log('A little about me:', me);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+  // added new property
+me.favoriteColor = "Blue"
+
+  // console.log the Object
+console.log(me) // An Object containing favoriteColor-Blue pair 
+
+ // Console.log the value 
+console.log(me.favoriteColor) // Blue
